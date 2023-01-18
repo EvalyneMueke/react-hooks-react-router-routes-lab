21b66d9a9a0fae233989e2877496e210f95f3d3c
Evalyne <evalyne.mueke@student.moringaschool.com>
2023-01-18 16:14:11 +0300

    Completed lab

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,20 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const director=directors.map((dirt,index) =>(
+    <div key={index}>
+      {dirt.name}
+      <ul>
+        {dirt.movies.map((movie,index)=>(
+          <li key={index}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ))
+  return <div>
+    <h1>Directors Page</h1>
+    {director}
+  </div>;
 }
 
 export default Directors;
